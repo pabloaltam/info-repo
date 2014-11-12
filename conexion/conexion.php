767ddc8046bbfb78120f239f6610ea07b12ff832
sarqo <pabloaltamver@gmail.com>
2014-11-12 12:24:23 -0300

    arreglado conexion.php
    Se modificó la cadena de conexión, para trabajar con la base de datos
    online.

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conex = mysqli_connect("localhost","root","root",'u332152429_info');
+$conex = mysqli_connect("mysql8.000webhost.com","a2962834_infored","inacap2020","a2962834_infored");
 if ($conex->connect_errno) {
     echo "Fallo al contenctar a MySQL: (" . $conex->connect_errno . ") " . $conex->connect_error;
 }
