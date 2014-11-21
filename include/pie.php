a769bee6c14daa1ea277cdd1e1d0e953e5191de4
Lackfer <me@lackfer.com>
2014-11-21 11:22:57 -0300

    Revert "Modificada ruta del estilo"
    This reverts commit 1d4d788e5f18358e47339dafa802a4e2abc940e4.

--- include/pie.php
+++ include/pie.php
@@ -1,6 +1,6 @@
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>  
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="/css/estilo.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
   </body>
